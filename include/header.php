86f1214111c89da12d5702293d23b13a911ee3fb
Deepak.K.V <deepudeepz383@gmail.com>
2020-01-30 23:06:23 +0530

    first commint

---
+++ include/header.php
@@ -0,0 +1,18 @@
+<meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Nadar || Superstore</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="images/icons/icon_logo.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/ionicons.min.css">
+        <link rel="stylesheet" href="css/css-plugins-call.css">
+        <link rel="stylesheet" href="css/bundle.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link rel="stylesheet" href="css/colors.css">
