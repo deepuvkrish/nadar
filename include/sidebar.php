76192edb134ea9b3370bf297f3ec4ab06e93852c
Akhil <akhilaki322@gmail.com>
2020-02-22 09:57:48 +0530

    jkhkj

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,62 +1,33 @@
 <ul class="product-categories">
                                         <li class="cat-item">
-                                            <a href="shop.php">Vegitables</a>
-                                            <span class="count">(5)</span>
+                                            <a href="shop.php?p1=veg">Vegitables</a>
                                         </li>
                                         <li class="cat-item">
-                                            <a href="shop.php">Bedroom</a>
-                                            <span class="count">(4)</span>
+                                            <a href="shop.php?p1=fru">Fruits</a>
                                         </li>
                                         <li class="cat-item">
-                                            <a href="shop.php">Decor &amp; Furniture</a>
-                                            <span class="count">(12)</span>
+                                            <a href="shop.php?p1=bak">Bakery</a>
                                         </li>
                                         <li class="cat-item">
-                                            <a href="shop.php">Electronics &amp; Computer</a>
-                                            <span class="count">(13)</span>
+                                            <a href="shop.php?p1=bev">Beverages</a>
                                         </li>
                                         <li class="cat-item">
-                                            <a href="shop.php">Fashion &amp; clothings</a>
-                                            <span class="count">(13)</span>
+                                            <a href="shop.php?p1=gro">Grossery</a>
                                         </li>
                                         <li class="cat-item">
-                                            <a href="shop.php">Furniture</a>
-                                            <span class="count">(4)</span>
+                                            <a href="shop.php?p1=cos">Cosmetics</a>
                                         </li>
                                         <li class="cat-item">
-                                            <a href="shop.php">Home, Garden &amp; Tools</a>
-                                            <span class="count">(14)</span>
+                                            <a href="shop.php?p1=sta">Stationary</a>
                                         </li>
                                         <li class="cat-item">
-                                            <a href="shop.php">Laptops &amp; Desktops</a>
-                                            <span class="count">(7)</span>
+                                            <a href="shop.php?p1=pet">Pet foods</a>
                                         </li>
                                         <li class="cat-item">
-                                            <a href="shop.php">Livingroom</a>
-                                            <span class="count">(4)</span>
+                                            <a href="shop.php?p1=hous">Household</a>
                                         </li>
                                         <li class="cat-item">
-                                            <a href="#">Men</a>
-                                            <span class="count">(4)</span>
+                                            <a href="shop.php?p1=oth">others</a>
                                         </li>
-                                        <li class="cat-item">
-                                            <a href="#">Mobiles &amp; Tablets</a>
-                                            <span class="count">(6)</span>
-                                        </li>
-                                        <li class="cat-item">
-                                            <a href="#">Sport &amp; Outdoors</a>
-                                            <span class="count">(7)</span>
-                                        </li>
-                                        <li class="cat-item">
-                                            <a href="#">Toy, Kids &amp; Baby</a>
-                                            <span class="count">(7)</span>
-                                        </li>
-                                        <li class="cat-item">
-                                            <a href="#">Uncategorized</a>
-                                            <span class="count">(0)</span>
-                                        </li>
-                                        <li class="cat-item">
-                                            <a href="#">Women</a>
-                                            <span class="count">(4)</span>
-                                        </li>
+                                        
                                     </ul>
