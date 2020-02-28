7f8ef92cf6e698d51ed0f0df216bd6123725e44b
Deepak.K.V <deepudeepz383@gmail.com>
2020-02-28 10:41:17 +0530

    shop.php updated

---
+++ index99.php
@@ -0,0 +1,253 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <?php include "include/header.php";?>    
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+        <!-- Body main wrapper start -->
+        <div class="wrapper home-one">
+            <?php include "include/navbar.php";?>
+            <!-- Slider area -->
+            <div class="slider-area">   
+                <div class="container">
+                    <div class="row">
+                        <div class="slider-hidden col-lg-3">
+                            <div class="sidebar-product-categori">
+                                <div class="widget-title">
+                                    <h3>PRODUCT CATEGORIES</h3>
+                                </div>
+                                <div class="widget-content">
+                                    <?php include "include/sidebar.php" ; ?>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="slider col-xl-9">
+						
+							<!-- slider-area start -->
+							<div class="slider-area-inner">
+								<!-- slider start -->
+								<div class="slider-inner">
+                                
+									<div id="mainSlider" class="nivoSlider nevo-slider">
+                                    
+                                    
+										<img src="images/slider/ca1.jpg" alt="main slider" title="#htmlcaption1"/>
+                                        <img src="images/slider/ca2.jpg" alt="main slider" title="#htmlcaption2"/>
+                                        <img src="images/slider/ca3.jpg" alt="main slider" title="#htmlcaption3"/>
+									</div>	
+
+									<div id="htmlcaption1" class="nivo-html-caption slider-caption">
+										<div class="slider-progress"></div>
+										<div class="col-md-9">
+											<div class="slider-content slider-content-1 slider-animated-1">
+												<h1 class="hone">FRESH</h1>
+												<h1 class="htwo">VEGGIES</h1>
+												<h1 class="hthree">FRUITS</h1>
+												<h3>WHOLESALE WITH RETAIL</h3> 
+												<div class="button-1 hover-btn-2">
+													<a href="shop.html">SHOP NOW</a>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div id="htmlcaption3" class="nivo-html-caption slider-caption">
+										<div class="slider-progress"></div>
+										<div class="col-md-9">
+											<div class="slider-content slider-content-3 slider-animated-3">
+												<h1 class="hone">INTERNATIONAL</h1>
+												<h1 class="htwo">BRANDS</h1>
+												<h3 class="h3one">UPTO 20% OFF</h3>
+												<h3 class="h3two">Your Own PERFUMES!</h3>
+												<div class="button-1 hover-btn-1">
+													<a href="shop.html">SHOP NOW</a>
+												</div>
+											</div>
+										</div>					
+                                    </div>
+                                    <div id="htmlcaption2" class="nivo-html-caption slider-caption">
+										<div class="slider-progress"></div>
+										<div class="col-md-9">
+											<div class="slider-content slider-content-2 slider-animated-2">
+												<h1 class="hone">BRANDS</h1>
+												<h1 class="htwo">LABELS</h1>
+												<h3 class="h3one">OF YOUR CHOICE</h3>
+												<h3 class="h3two">UPTO 30% OFF</h3>
+												<div class="button-1 hover-btn-1">
+													<a href="shop.html">SHOP NOW</a>
+												</div>
+											</div>
+										</div>					
+									</div>
+								</div>
+								<!-- slider end -->
+							</div>
+							<!-- slider-area end -->
+                        </div>
+                        <div class="slider-banner-area col-sm-12">
+                            <div class="slider-banner">
+                                <div class="slider-single-banner">
+                                    <a href="shop.html">
+                                        <img src="images/banner/banner1.jpg" alt="Banner">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="slider-banner">
+                                <div class="slider-single-banner">
+                                    <a href="shop.html">
+                                        <img src="images/banner/banner2.jpg" alt="Banner">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Slider area end -->
+            <!-- Policy area -->
+            <div class="policy-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="policy-area-inner">
+                                <div class="single-policy-area">
+                                    <div class="single-policy">
+                                        <div class="icon"><i class="icon ion-android-sync"></i></div>
+                                        <h3>Free Return</h3>
+                                        <p>30 days money back guarantee!</p>
+                                    </div>
+                                </div>
+                                <div class="single-policy-area">
+                                    <div class="single-policy">
+                                        <div class="icon"><i class="icon ion-paper-airplane"></i></div>
+                                        <h3>Free Shipping</h3>
+                                        <p>Free shipping on all order</p>
+                                    </div>
+                                </div>
+                                <div class="single-policy-area">
+                                    <div class="single-policy">
+                                        <div class="icon"><i class="icon ion-help-buoy"></i></div>
+                                        <h3>Support 24/7</h3>
+                                        <p>We support online 24 hrs a day</p>
+                                    </div>
+                                </div>
+                                <div class="single-policy-area">
+                                    <div class="single-policy">
+                                        <div class="icon"><i class="icon ion-card"></i></div>
+                                        <h3>Receive Gift Card</h3>
+                                        <p>Recieve gift all over oder $50</p>
+                                    </div>
+                                </div>
+                                <div class="single-policy-area">
+                                    <div class="single-policy">
+                                        <div class="icon"><i class="icon ion-ios-locked"></i></div>
+                                        <h3>Secure Payment</h3>
+                                        <p>We Value Your Security</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>   
+            <!-- Policy area end -->
+            
+            
+            
+            <!-- Brand logo area -->
+            <div class="brand-logo-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="brand-carousel-active owl-carousel">
+                                <!-- single brand logo -->
+                                <div class="brand-logo">
+                                    <a href="#"><img src="images/brand/brand1.png" alt="Brand logo"></a>
+                                </div>
+                                <!-- single brand logo end -->
+                                <!-- single brand logo -->
+                                <div class="brand-logo">
+                                    <a href="#"><img src="images/brand/brand2.png" alt="Brand logo"></a>
+                                </div>
+                                <!-- single brand logo end -->
+                                <!-- single brand logo -->
+                                <div class="brand-logo">
+                                    <a href="#"><img src="images/brand/brand3.png" alt="Brand logo"></a>
+                                </div>
+                                <!-- single brand logo end -->
+                                <!-- single brand logo -->
+                                <div class="brand-logo">
+                                    <a href="#"><img src="images/brand/brand4.png" alt="Brand logo"></a>
+                                </div>
+                                <!-- single brand logo end -->
+                                <!-- single brand logo -->
+                                <div class="brand-logo">
+                                    <a href="#"><img src="images/brand/brand5.png" alt="Brand logo"></a>
+                                </div>
+                                <!-- single brand logo end -->
+                                <!-- single brand logo -->
+                                <div class="brand-logo">
+                                    <a href="#"><img src="images/brand/brand6.png" alt="Brand logo"></a>
+                                </div>
+                                <!-- single brand logo end -->
+                                <!-- single brand logo -->
+                                <div class="brand-logo">
+                                    <a href="#"><img src="images/brand/brand7.png" alt="Brand logo"></a>
+                                </div>
+                                <!-- single brand logo end -->
+                                <!-- single brand logo -->
+                                <div class="brand-logo">
+                                    <a href="#"><img src="images/brand/brand8.png" alt="Brand logo"></a>
+                                </div>
+                                <!-- single brand logo end -->
+                                <!-- single brand logo -->
+                                <div class="brand-logo">
+                                    <a href="#"><img src="images/brand/brand9.png" alt="Brand logo"></a>
+                                </div>
+                                <!-- single brand logo end -->
+                                <!-- single brand logo -->
+                                <div class="brand-logo">
+                                    <a href="#"><img src="images/brand/brand10.png" alt="Brand logo"></a>
+                                </div>
+                                <!-- single brand logo end -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php include "include/footer.php" ;?>
+        </div>
+        <!-- Body main wrapper end -->
+
+
+        <!-- jQuery CDN -->
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <!-- jQuery Local -->
+        <script>window.jQuery || document.write('<script src="js/jquery-3.2.1.min.js"><\/script>')</script>
+
+        <!-- Popper min js -->
+        <script src="js/popper.min.js"></script>
+        <!-- Bootstrap min js  -->
+        <script src="js/bootstrap.min.js"></script>
+		<!-- nivo slider pack js  -->
+        <script src="js/jquery.nivo.slider.pack.js"></script>
+        <!-- All plugins here -->
+        <script src="js/plugins.js"></script>
+        <!-- Main js  -->
+        <script src="js/main.js"></script>
+
+
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+</html>
